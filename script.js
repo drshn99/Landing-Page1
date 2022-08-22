7e235ec368bd3cf441d896157c92bfc035d163e4
drshn99 <darshanpatel2001.com>
2022-08-22 12:54:12 +0530

    qwerty

---
+++ script.js
@@ -0,0 +1,8 @@
+function imgslider(anything) {
+  document.querySelector(".starbucks").src = anything;
+}
+
+function changecirclecolor(color) {
+  const circle = document.querySelector(".circle");
+  circle.style.background = color;
+}
